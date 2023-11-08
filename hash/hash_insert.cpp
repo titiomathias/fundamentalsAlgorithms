3fea636ad35abaf01132eac7555fcddd1bae8c68
Matheus de Alencar Costa Oliveira <matheus.alencar@alunos.ifsuldeminas.edu.br>
2023-11-08 16:33:27 -0300

    Add files via upload

---
+++ hash/hash_insert.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+using namespace std;
+
+struct dado{
+	int valor;
+	int status;
+};
+
+int hash(int k, int m){
+	return k%m;
+}
+
+int hashLinear(int k, int i, int m){
+	return (hash(k, m)+i)%m;
+}
+
+int hash_insert(dado vetor[], int m, int k){
+	int i = 0;
+	int j = 0;
+	
+	do{
+		j = hashLinear(k, i, m);
+		if(vetor[j].status != 1){
+			vetor[j].valor = k;
+			vetor[j].status = 1;
+			return j;
+		} else {
+			i++;
+		}
+	}while(i!=m);
+	
+	return -1;
+}
+
+int main(){
+	int m, k;
+	
+	cin >> m;
+	
+	dado vetor[m];
+	
+	for(int i = 0; i < m; i++){
+		vetor[i].status = -1;
+		vetor[i].valor = -1;
+	}
+	
+	int a = 0; 
+	
+	cin >> k;
+	
+	do{
+		hash_insert(vetor, m, k);
+		a++;
+		cin >> k;
+	}while(k!=0 && a!=m);
+	
+	for(int i = 0; i < m; i++){
+		cout << vetor[i].valor << " ";	
+	}
+	
+	return 0;
+}
